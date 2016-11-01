2f14f4772460274a17c5197b8b056d451a304744
Shin <tioncai@gmail.com>
2016-11-01 17:54:04 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,6 @@ if [ ! -f "/var/lib/mysql/init" ]; then
   fi
   echo "set password for root@localhost = password('$MYSQL_ROOT_PASSWORD')" | mysql
   echo "set password for root@127.0.0.1 = password('$MYSQL_ROOT_PASSWORD')" | mysql -uroot -p$MYSQL_ROOT_PASSWORD
-  echo "create database wordpress" | mysql -uroot -p$MYSQL_ROOT_PASSWORD
   touch /var/lib/mysql/init
 else
   mysqld_safe &
