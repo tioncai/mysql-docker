11dbf050b06bcc267a33245961c3875366c8885d
Shin <tioncai@gmail.com>
2016-11-01 19:44:39 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ if [ ! -f "/var/lib/mysql/init" ]; then
     MYSQL_ROOT_PASSWORD="ghostcloud"
   fi
   echo "set password for root@localhost = password('$MYSQL_ROOT_PASSWORD')" | mysql
-  echo "set password for root@127.0.0.1 = password('$MYSQL_ROOT_PASSWORD')" | mysql -uroot -p$MYSQL_ROOT_PASSWORD
+  echo "set password for root@% = password('$MYSQL_ROOT_PASSWORD')" | mysql -uroot -p$MYSQL_ROOT_PASSWORD
   touch /var/lib/mysql/init
 else
   mysqld_safe
