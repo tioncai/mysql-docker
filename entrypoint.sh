a14613de724406238e4df96f7c118f5477a2af69
Shin <tioncai@gmail.com>
2016-11-17 11:17:15 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,8 @@ if [ ! -f "/var/lib/mysql/init" ]; then
   fi
   echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;" | mysql
   echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;" | mysql -uroot -p$MYSQL_ROOT_PASSWORD
-  //echo "set password for root@localhost = password('$MYSQL_ROOT_PASSWORD')" | mysql
-  //echo "set password for root@% = password('$MYSQL_ROOT_PASSWORD')" | mysql -uroot -p$MYSQL_ROOT_PASSWORD
+  #echo "set password for root@localhost = password('$MYSQL_ROOT_PASSWORD')" | mysql
+  #echo "set password for root@% = password('$MYSQL_ROOT_PASSWORD')" | mysql -uroot -p$MYSQL_ROOT_PASSWORD
   touch /var/lib/mysql/init
 else
   mysqld_safe
