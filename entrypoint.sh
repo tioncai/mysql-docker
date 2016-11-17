70150fb6b263c0ab364344333e8480cb1cf94fda
Shin <tioncai@gmail.com>
2016-11-17 11:16:52 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,10 @@ if [ ! -f "/var/lib/mysql/init" ]; then
   if [ -z "$MYSQL_ROOT_PASSWORD" ]; then
     MYSQL_ROOT_PASSWORD="ghostcloud"
   fi
-  echo "set password for root@localhost = password('$MYSQL_ROOT_PASSWORD')" | mysql
-  echo "set password for root@% = password('$MYSQL_ROOT_PASSWORD')" | mysql -uroot -p$MYSQL_ROOT_PASSWORD
+  echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;" | mysql
+  echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;" | mysql -uroot -p$MYSQL_ROOT_PASSWORD
+  //echo "set password for root@localhost = password('$MYSQL_ROOT_PASSWORD')" | mysql
+  //echo "set password for root@% = password('$MYSQL_ROOT_PASSWORD')" | mysql -uroot -p$MYSQL_ROOT_PASSWORD
   touch /var/lib/mysql/init
 else
   mysqld_safe
